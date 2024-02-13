412904c82a9a620a7263cdab1699a35713eeccd0
Hawks <126882395+h4wks123@users.noreply.github.com>
2024-02-13 23:54:01 +0800

    [BAYER][Added login page]

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,125 +1,157 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'customtextformfield.dart';
+import 'primarybutton.dart';
+import 'passwordfield.dart';
+import 'routes.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(
+    home: LoginScreen(),
+    routes: routes,
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class LoginScreen extends StatelessWidget {
+  static const String routeName = "login";
+  const LoginScreen({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return Scaffold(
+      body: Center(
+        child: _LoginScreenBody(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      bottomNavigationBar: _LoginFooter(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class _LoginScreenBody extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<_LoginScreenBody> createState() => _LoginScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _LoginScreenState extends State<_LoginScreenBody> {
+  bool obscureText = true;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.green[200],
+        body: Container(
+          color: Color(0xFFFFF5E1),
+          alignment: Alignment.topCenter,
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                WebsiteLogo(),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                const Text(
+                  'Sign Up',
+                  style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.w800,
+                      color: Color(0xFF74382C)),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                const CustomTextFormField(
+                  labelText: "Email Address",
+                  hintText: "Enter a valid email",
+                  iconData: Icons.email,
+                  textInputType: TextInputType.emailAddress,
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                PasswordField(
+                  labelText: "Password",
+                  hintText: "Enter your password",
+                  obscureText: obscureText,
+                  onTap: setPasswordVisibility,
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                PrimaryButton(
+                  text: "Login",
+                  iconData: Icons.login,
+                  onPressed: login,
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+        ),
+      ),
+    );
+  }
+
+  void login() {
+    // Navigator.pushNamed(context, HomeScreen.routeName);
+  }
+
+  void setPasswordVisibility() {
+    setState(() {
+      obscureText = !obscureText;
+    });
+  }
+}
+
+class _LoginFooter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xFFFFF5E1),
+      height: 35.0,
+      child: Text.rich(
+        TextSpan(
+          text: 'Not yet a member?',
+          style: TextStyle(
+            color: Color(0xFF74382C),
+            fontSize: 17.0,
+          ),
+          children: <TextSpan>[
+            TextSpan(
+              text: ' Register now',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Color(0xff84bd94),
+              ),
+              recognizer: TapGestureRecognizer()
+                ..onTap = () {
+                  print('Register now tapped!');
+                },
             ),
           ],
         ),
+        textAlign:
+            TextAlign.center,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class WebsiteLogo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(height: 10.0),
+        Image.asset(
+          'assets/website_logo.png',
+          width: 300.0,
+          height: 300.0,
+        ),
+      ],
     );
   }
 }
---
+++ flutter_application_1/lib/routes.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  LoginScreen.routeName: (BuildContext context) => LoginScreen(),
+};
+
+
+/*
+Color codes:
+Tier 1 = 0xFFFFF5E1 Lightbrown
+Tier 2 = 0xFF74382C Brown
+Tier 3 = 0xff84bd94 Green
+Tier 4 = 0xFFEF8829 Orange
+Tier 5 = 0xFF808080 Gray
+*/
